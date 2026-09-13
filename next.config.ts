0f335fbcbfc64b08b7b625547565b0d58dd5cb0c
bahbood <honarmand.mahmood@hotmail.com>
2026-09-13 12:09:23 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+
   outputFileTracingIncludes: {
     "**/*": [
       "./node_modules/pg-cloudflare/dist/**",
       "./node_modules/pg-cloudflare/esm/**",
     ],
   },
-
-  /* config options here */
-  // cacheComponents: true,
 };
 
 export default nextConfig;
